9d5db2fcc4e673fd7bf5a73af11471eeabb5703a
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2023-09-08 01:13:34 -0300

    Update Mana.cs

--- Assets/Mana.cs
+++ Assets/Mana.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mana : MonoBehaviour
 {
+    public ManaPlay mana;
     public GameObject bulletPrefab;
     public Transform shootingPoint;
+    public float manaMana;
 
-    void Start()
+    private void Awake()
     {
-        
+        mana = new ManaPlay();
     }
 
     void Update()
     {
+        mana.Update();
+
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Shoot();
+            if (mana.manaAmount > 20)
+            {
+                mana.TrySpend(20);
+                Shoot();
+            }
         }
     }
 
@@ -24,5 +33,39 @@ public class Mana : MonoBehaviour
     {
         shootingPoint.rotation = gameObject.transform.rotation;
         Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
+    }
+
+    public class ManaPlay
+    {
+        public const int MANA_MAX = 100;
+
+        public float manaAmount;
+        public float manaRegenAmount;
+
+        public ManaPlay()
+        {
+
+            manaAmount = 0;
+            manaRegenAmount = 15f;
+        }
+
+        public void Update()
+        {
+            manaAmount += manaRegenAmount * Time.deltaTime;
+            manaAmount = Mathf.Clamp(manaAmount, 0f, MANA_MAX);
+        }
+
+        public void TrySpend(int amount)
+        {
+            if (manaAmount >= amount)
+            {
+                manaAmount -= amount;
+            }
+        }
+
+        public float GetManaNormalized()
+        {
+            return manaAmount / MANA_MAX;
+        }
     }
 }
