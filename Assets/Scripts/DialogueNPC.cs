906572b0b1964cdbc84c863881c2e8a61ac3a893
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2023-11-02 00:15:26 -0300

    UI and Dialogue (Still with Bugs)
    Bug 1 - Wall Jump isn't happening how It's supposed to, the player isn't moving away from the wall
    Bug 2 - During the Dialogue the state isn't changing to "DoNothing"

---
+++ Assets/Scripts/DialogueNPC.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Xml.Serialization;
+
+public class DialogueNPC : MonoBehaviour
+{
+    [Header("Dialogue Mission Imcomplete")]
+    public string[] dialogueNpc;
+    public int dialogueIndex;
+
+    [Header("NPC Name")]
+
+    public string nameNpcWrite;
+
+    [Header("UI")]
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public TextMeshProUGUI nameNpc;
+    public Image imageNpc;
+    public GameObject exclamationMark;
+
+    [Header("Bools")]
+    public bool readyToTalk;
+    public bool startDialogue;
+    public bool firstTalk = true;
+    public bool dontTalkAgaing = false; 
+
+    void Update()
+    {
+        //Codigo para que o player tenha que apertar para interagir 
+        //if (Input.GetKey(KeyCode.E))
+        //{
+        //    //if (!startDialogue && (FindObjectOfType<MoveFSM>().IsGrounded() == true) && readyToTalk)
+        //    //{
+        //    //    FindObjectOfType<MoveFSM>().doNothing = true;
+        //    //    firstTalk = false;
+        //    //    StartDialogue();
+        //    //}
+        //}
+
+        if (dialogueText.text == dialogueNpc[dialogueIndex] && Input.GetKey(KeyCode.Space))
+        {
+            NextDialogue();
+        }
+    }
+
+    void StartDialogue()
+    {
+        startDialogue = true;
+        nameNpc.text = "" + nameNpcWrite;
+        dialogueIndex = 0;
+        dialoguePanel.SetActive(true);
+        exclamationMark.SetActive(false);
+        StartCoroutine(ShowDialogue());
+    }
+
+    void NextDialogue()
+    {
+        dialogueIndex = dialogueIndex + 1;
+
+        if (dialogueIndex < dialogueNpc.Length)
+        {
+            StartCoroutine(ShowDialogue());
+        }
+        if (dialogueIndex == dialogueNpc.Length)
+        {
+            readyToTalk = false;
+            dialoguePanel.SetActive(false);
+            startDialogue = false;
+            dialogueIndex = 0;
+            FindObjectOfType<MoveFSM>().doNothing = false;
+        }
+    }
+
+    IEnumerator ShowDialogue()
+    {
+        dialogueText.text = "";
+
+        foreach (char letter in dialogueNpc[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            readyToTalk = true;
+        }
+        else
+        {
+            readyToTalk = false;
+        }
+
+        if (!startDialogue && (FindObjectOfType<MoveFSM>().IsGrounded() == true) && readyToTalk && !dontTalkAgaing)
+        {
+            FindObjectOfType<MoveFSM>().doNothing = true;
+            firstTalk = false;
+            StartDialogue();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            dontTalkAgaing = true;
+        }
+    }
+}
