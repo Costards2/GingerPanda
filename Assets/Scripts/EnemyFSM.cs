fac4183321b97ba38e197d1dd7c043c6c3562fbd
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2023-11-16 14:20:45 -0300

    Enemy KnockBack Added + Damage "Animation"

--- Assets/Scripts/EnemyFSM.cs
+++ Assets/Scripts/EnemyFSM.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class EnemyFSM : MonoBehaviour
 {
     public int maxHealth = 100;
     int currentHealth;
     public float tempo = 1;
+    private SpriteRenderer sprite;
+    private Color normalColor;
+    bool isKb;
+    private readonly float kbForceX = 6f;
+    private readonly float kbForceY = 0f;
+    private Rigidbody2D rb; 
 
     public float walkSpeed = 5f;
     private float currentWalkSpeed; 
@@ -18,18 +25,23 @@ public class EnemyFSM : MonoBehaviour
     private enum EnemyState
     {
         WalkingRight,
-        WalkingLeft
+        WalkingLeft,
+        TakeDamage
     }
 
     private EnemyState currentState;
+    private EnemyState beforeState;
 
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
+        normalColor = sprite.color;
         currentWalkSpeed = walkSpeed;
         currentHealth = maxHealth;
         currentState = EnemyState.WalkingRight;
         right = pointRight.position.x;
         left = pointLeft.position.x;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -76,17 +88,69 @@ public class EnemyFSM : MonoBehaviour
         walkSpeed = currentWalkSpeed; 
     }
 
+    IEnumerator DamageWait()
+    {
+        walkSpeed = 0;
+        yield return new WaitForSeconds(0.3f);
+        walkSpeed = currentWalkSpeed;
+    }
+
+    public IEnumerator Damage()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            sprite.color = new Color(0.68f, 0.17f, 0.17f, 0.90f);
+
+            //sprite.enabled = true;
+
+            yield return new WaitForSeconds(0.15f);
+
+            sprite.color = normalColor;
+
+            //sprite.enabled = false; 
+
+            yield return new WaitForSeconds(0.15f);
+
+            //sprite.enabled = true;
+        }
+    }
+
     public void TakeDamage(int damage)
     {
+        beforeState = currentState; 
         currentHealth -= damage;
+        StartCoroutine(DamageWait());
+        StartCoroutine(Damage());
 
         //Hurt anim 
+
+        isKb = true;
 
         if (currentHealth < 0)
         {
             Die();
         }
+        if (isKb)
+        {
+            if (currentState == EnemyState.WalkingRight)
+            {
+                rb.velocity = new Vector2(-kbForceX, kbForceY);
+                isKb = false;
+            }
+            else if (currentState == EnemyState.WalkingLeft)
+            {
+                rb.velocity = new Vector2(kbForceX, kbForceY);
+                isKb = false;
+            }
+        }
 
+        Invoke(nameof(StopKB), 0.15f);
+    }
+
+    void StopKB()
+    {
+        rb.velocity = new Vector2(0, 0);
+        currentState = beforeState;
     }
 
     void Die()
