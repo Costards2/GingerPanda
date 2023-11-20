0565e0fe2275c0359d60a518bca607447d55369b
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2023-11-19 21:04:18 -0300

    Some Bug Fixes
    Also, I tried to make the laser but It went pretty wrong

---
+++ Assets/Boss.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+
+        currentHealth -= damage;
+       
+
+        //Hurt anim 
+
+        if (currentHealth < 0)
+        {
+            Die();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void Die()
+    {
+        //Die anim
+
+        Destroy(gameObject);
+    }
+}
